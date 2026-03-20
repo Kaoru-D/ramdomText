ba33c01233f419efa3981f01621700dbced3340e
DanielK <daniel.rt5d@gmail.com>
2026-03-20 14:56:20 -0500

    feat: implement text generator with user input and dynamic display

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import data from './data';
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0,amount));
+  }
+
+  return (
+    <section className="section-center">
+      <h4> ¿Cansado de textos génericos y aburridos? </h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">párrafos</label>
+        <input type="number" name="amount" id="amount" min='1' step='1' max='14' value={count} onChange={(e)=>{setCount(e.target.value)}}/>
+        <button type="submit" className="btn">
+          Generar
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item)=>{
+          return <p key={nanoid()}>{item}</p>
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
